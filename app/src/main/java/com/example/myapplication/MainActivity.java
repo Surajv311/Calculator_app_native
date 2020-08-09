c183568feae9133727dbc1531b3282670ab34475
Surajv <vsuraj2405@gmail.com>
2020-08-09 20:11:35 +0530

    added functionality for operations

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -10,8 +11,8 @@ public class MainActivity extends AppCompatActivity {
 Button b1, b2 , b3 , b4 , b5 , b6 , b7 , b8 , b9 , b0 , bAdd , bSubtract , bMultiply , bDivide , bEqual , bDot ;
 Button bClear ;
 EditText ed1 ;
-float Res1 , Res2 ;
-boolean Add , Sub , Mul , Div ;
+float res1 , res2 ;
+boolean Add , Subtract , Multiply , Divide ;
 
 
 
@@ -40,10 +41,151 @@ boolean Add , Sub , Mul , Div ;
         bEqual = (Button)findViewById(R.id.bEqual);
         ed1 = (EditText)findViewById(R.id.editText2);
 
+        // now for action on click
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // setting the value 1 whatever you put in the edit box
+                ed1.setText(ed1.getText() + "1");
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // setting the value 1 whatever you put in the edit box
+                ed1.setText(ed1.getText() + "2");
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // setting the value 1 whatever you put in the edit box
+                ed1.setText(ed1.getText() + "3");
+            }
+        });
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // setting the value 1 whatever you put in the edit box
+                ed1.setText(ed1.getText() + "4");
+            }
+        });
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // setting the value 1 whatever you put in the edit box
+                ed1.setText(ed1.getText() + "5");
+            }
+        });
+        b6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // setting the value 1 whatever you put in the edit box
+                ed1.setText(ed1.getText() + "6");
+            }
+        });
+        b7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // setting the value 1 whatever you put in the edit box
+                ed1.setText(ed1.getText() + "7");
+            }
+        });
+        b8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // setting the value 1 whatever you put in the edit box
+                ed1.setText(ed1.getText() + "8");
+            }
+        });
+        b9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // setting the value 1 whatever you put in the edit box
+                ed1.setText(ed1.getText() + "9");
+            }
+        });
+        b0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // setting the value 1 whatever you put in the edit box
+                ed1.setText(ed1.getText() + "0");
+            }
+        });
+        bDot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // setting the value 1 whatever you put in the edit box
+                ed1.setText(ed1.getText() + ".");
+            }
+        });
+
+
+        bAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ed1==null){
+                    ed1.setText("");
+                }
+                else{
+                    res1 = Float.parseFloat(ed1.getText() + "");
+                    // now when '+' pressed then the text box empty & the variable stored in 'Add' ...
+                    Add = true;
+                    ed1.setText(null);
+                }
+            }
+        });
+
+        bSubtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ed1==null){
+                    ed1.setText("");
+                }
+                else{
+                    res1 = Float.parseFloat(ed1.getText() + "");
+                    Subtract = true;
+                    ed1.setText(null);
+                }
+            }
+        });
+
+        bMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ed1==null){
+                    ed1.setText("");
+                }
+                else{
+                    res1 = Float.parseFloat(ed1.getText() + "");
+                    Multiply = true;
+                    ed1.setText(null);
+                }
+            }
+        });
+
+        bDivide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ed1==null){
+                    ed1.setText("");
+                }
+                else{
+                    res1 = Float.parseFloat(ed1.getText() + "");
+                    Divide = true;
+                    ed1.setText(null);
+                }
+            }
+        });
 
 
 
 
 
+
+
+        
     }
 }
